25933256cf5e43645842d7ca334ab6078d0b4010
st133 <s_t133@student.usc.edu.au>
2019-08-10 19:05:41 +1000

    updateQuestion method added and encapsulated to mNextButton's listener.

--- app/src/main/java/android/bignerdranch/com/QuizActivity.java
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -29,8 +29,6 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +46,21 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 }
